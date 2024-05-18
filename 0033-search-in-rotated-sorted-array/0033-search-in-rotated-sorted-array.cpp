23a5a52763a63f93c6510de577acd6bf7f107c74
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-05-18 07:03:29 +0530

    Time: 8 ms (10.01%), Space: 13.5 MB (35.35%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -11,19 +11,19 @@ public:
             }
             //left side sorted
             else if(arr[mid]>=arr[0]){
-                if(arr[start]<=target && target<=arr[mid])
+                if(arr[start]<=target && target<=arr[mid])// check whether target is present between start and mid or not
                 end=mid-1;
                 else 
                 start=mid+1;
             }
             // right side sorted
             else{
-            if(arr[mid]<target && target<=arr[end])
+            if(arr[mid]<target && target<=arr[end])// check whether target is present  between mid and end or not
               start=mid+1;  
             else
             end=mid-1;
             }
          }
-         return -1;
+         return -1; // if target not found return -1
     }
 };
