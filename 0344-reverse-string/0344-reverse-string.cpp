6c38899a4fd3b592005a4339d1f229fed42e76bd
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-06-03 06:56:56 +0530

    Time: 7 ms (99.29%), Space: 27 MB (60.05%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
 void reverseString(vector<char>& s) {
-int n=s.size();
-for(int i=0;i<n/2;i++){
- swap(s[i],s[n-1-i]);
- }
- }
+// int n=s.size();
+// for(int i=0;i<n/2;i++){
+// swap(s[i],s[n-1-i]);
+reverse(s.begin(),s.end());
+}
+// }
 };
