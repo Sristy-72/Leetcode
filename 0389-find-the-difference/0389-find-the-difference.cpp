cdbddbc797efcb33306ff67f8a6e6e50fb1d9f54
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-07-01 13:49:32 +0530

    Time: 0 ms (100%), Space: 7.9 MB (56.9%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-    unordered_map<char,int>mp;
-    for(char &ch:s){
-        mp[ch]++;
-    }    
-    for(char &ch:t){
-        // if(mp.find()==mp.end())
-        // return ch
-        mp[ch]--;
-        if(mp[ch]<0)
-        return ch;
+        // Approach 1(hashing)
+    // unordered_map<char,int>mp;
+    // for(char &ch:s){
+    //     mp[ch]++;
+    // }    
+    // for(char &ch:t){
+    //     // if(mp.find()==mp.end())
+    //     // return ch
+    //     mp[ch]--;
+    //     if(mp[ch]<0)
+    //     return ch;
+    // }
+    // return 'x';
+
+    // approach 2(sum)
+    int sum1=0;
+    for(char&ch :s){
+        sum1+=ch;
     }
-    return 'x';
+    int sum2=0;
+    for(char&ch:t){
+        sum2+=ch;
+    }
+    return (char)(sum2-sum1);
     }
 };
