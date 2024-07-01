21c1e890d3828c5c4c726dc4138af6a9c364b34c
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-07-01 07:27:36 +0530

    Time: 3 ms (60.88%), Space: 10.7 MB (8.09%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -5,13 +5,13 @@ public:
      int cnt=0;
      int n=arr.size();
      for(int i=0;i<n;i++){
-      if(arr[i]%2==1){
-        cnt++;
-      }
-      else{
-      cnt=0;// reset
-      }
-      if(cnt==3)
+    if(arr[i]%2==1){
+    cnt++;
+    }
+    else{
+    cnt=0;// reset
+    }
+    if(cnt==3)
 return true;
      }
         return false;
