ba1b662da1e8c839c3f2ed531dd556d7fec53171
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-07-02 10:10:06 +0530

    Time: 5 ms (38.58%), Space: 12.5 MB (75.54%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -3,24 +3,42 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
     
     
-    //       vector<int>v;
-    //       sort(nums1.begin(),nums1.end());
-    //        sort(nums2.begin(),nums2.end());
-    //  int n=nums1.size();
-    //  int m=nums2.size();
-    //  for(int i=0;i<n;i++){
-    //     for(int j=0;j<m;j++){
+          vector<int>ans;
+        //   sort(nums1.begin(),nums1.end());
+           sort(begin(nums1),end(nums1));// method 2 for sorting
+           sort(nums2.begin(),nums2.end());
+     int n=nums1.size();
+     int m=nums2.size();
+     int i=0;
+     int j=0;
+     while(i<n && j<m){
+       if(nums1[i]==nums2[j]){
+        ans.push_back(nums1[i]);
+        i++;
+        j++;
+       } 
+       else if(nums1[i]<nums2[j]){
+         i++;
+       }
+       else{
+        j++;
+       }
+     }
+     return ans;
+    
+    //  for(i=0;i<n;i++){
+    //     for( j=0;j<m;j++){
     //         if(nums1[i]==nums2[j]){
     //            v.push_back(nums1[i]);
     //         //    nums1[i]++;
-    //         nums1[i]++;
-    //         nums2[j]++;
+    //         i++;
+    //         j++;
     //         }
-    //         if(nums1[i]>nums2[j]){
-    //             nums2[j]++;
+    //         else if(nums1[i]>nums2[j]){
+    //             j++;
     //         }
     //         else{
-    //            nums2[j]++;
+    //            i++;
     //         }
             
     //     }
@@ -30,18 +48,18 @@ public:
     
 
     // method 1(hashing)
-    unordered_map<int,int>mp;
-    for(int &i:nums1){
-        mp[i]++;
-    }
-    vector<int>ans;
-    for(int i=0;i<nums2.size();i++){
-       int num=nums2[i];
-       if(mp[num]>0){
-        mp[num]--;
-        ans.push_back(num);
-       }
-    }
-    return ans;
+    // unordered_map<int,int>mp;
+    // for(int &i:nums1){
+    //     mp[i]++;
+    // }
+    // vector<int>ans;
+    // for(int i=0;i<nums2.size();i++){
+    //    int num=nums2[i];
+    //    if(mp[num]>0){
+    //     mp[num]--;
+    //     ans.push_back(num);
+    //    }
+    // }
+    // return ans;
     }
 };
