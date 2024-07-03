21f93297ba8a987cf8a711f088b3fbff23cbff8e
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-07-03 21:58:35 +0530

    Time: 70 ms (51.1%), Space: 38.8 MB (15.52%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -2,18 +2,26 @@ class Solution {
 public:
     int minDifference(vector<int>& nums) {
        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        if(n<=4){
+       int MAX_MOVE = 3; //changeable condition
+        if (nums.size() <= MAX_MOVE + 1)
             return 0;
-        }
-        //1st
-        int res=nums[n-1]-nums[3];
-        //2nd
-        res=min(res,nums[n-4]-nums[0]);
-        //3rd
-        res=min(res,nums[n-2]-nums[2]);
-        //4th
-        res=min(res,nums[n-3]-nums[1]);
-        return res;  
+        int n = nums.size();
+        int ans = INT_MAX;
+        for (int i = 0, j = n-1-MAX_MOVE; i <= MAX_MOVE; ++i, ++j)
+            ans = min(ans, nums[j] - nums[i]);
+        return ans;
+        // int n=nums.size();
+        // if(n<=4){
+        //     return 0;
+        // }
+        // //1st
+        // int res=nums[n-1]-nums[3];
+        // //2nd
+        // res=min(res,nums[n-4]-nums[0]);
+        // //3rd
+        // res=min(res,nums[n-2]-nums[2]);
+        // //4th
+        // res=min(res,nums[n-3]-nums[1]);
+        // return res;  
     }
 };
