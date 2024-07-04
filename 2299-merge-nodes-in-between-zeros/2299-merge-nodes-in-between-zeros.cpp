339c132163d82a741569ef20848e3c68b20a7c9e
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-07-04 11:08:35 +0530

    Time: 428 ms (69.98%), Space: 259.8 MB (67.77%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
@@ -11,19 +11,35 @@
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
-      ListNode*p1=head->next;
-       ListNode*p2=p1;
-       while(p2!=NULL){
-       int sum=0;
-       while(p2!=NULL && p2->val!=0){
-        sum=sum+p2->val;
-        p2=p2->next;
-       } 
-       p1->val=sum;
-       p2=p2->next;
-       p1->next=p2;
-       p1=p1->next;
-       }
-       return head->next;
+        // approach 1
+    //   ListNode*p1=head->next;
+    //    ListNode*p2=p1;
+    //    while(p2!=NULL){
+    //    int sum=0;
+    //    while(p2!=NULL && p2->val!=0){
+    //     sum=sum+p2->val;
+    //     p2=p2->next;
+    //    } 
+    //    p1->val=sum;
+    //    p2=p2->next;
+    //    p1->next=p2;
+    //    p1=p1->next;
+    //    }
+    //    return head->next;
+
+    // Approach 2(Recursion)
+    head=head->next;
+    if(head==NULL){
+        return head;
+    }
+    ListNode* temp=head;
+    int sum=0;
+    while(temp->val!=0 && temp!=NULL){ 
+        sum+=temp->val;
+        temp=temp->next;
+    }
+    head->val=sum;
+    head->next=mergeNodes(temp);// recursion leap of faith
+    return head;
     }
 };
