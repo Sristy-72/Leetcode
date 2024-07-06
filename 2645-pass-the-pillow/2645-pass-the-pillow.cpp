15cd76c38bed9285f761fb87beeda7fa7f352828
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-07-06 19:58:55 +0530

    Time: 0 ms (100%), Space: 7.1 MB (30.58%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -3,18 +3,29 @@ public:
     int passThePillow(int n, int time) {
      //first approach (simulation:=do what is asked)
     //  when we are going left to right then we will add and when we will move from right to left we will subtract
-     int pillowpos  = 1;
-        int dir  = 1;// left to right
+    //  int pillowpos  = 1;
+    //     int dir  = 1;// left to right
 
-        while(time > 0) {
-            if(pillowpos + dir >=1 && pillowpos + dir <= n) {
-                pillowpos += dir;
-                time--;
-            } else {
-                dir *= -1;
-            }
+    //     while(time > 0) {
+    //         if(pillowpos + dir >=1 && pillowpos + dir <= n) {
+    //             pillowpos += dir;
+    //             time--;
+    //         } else {
+    //             dir *= -1;
+    //         }
+    //     }
+    //     return pillowpos;   
+
+    // APPROACH:-2(simple maths)
+    int completeround=time/(n-1);
+    int timeleft=time % (n-1);
+    if(completeround % 2 == 0) {
+            return timeleft + 1;
+        } else {
+            return n - timeleft;
         }
-        return pillowpos;   
+
+        return -1;
 
     }
 };
