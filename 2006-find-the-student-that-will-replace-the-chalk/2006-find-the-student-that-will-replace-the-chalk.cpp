b00a9ee925e074b27ecf4d182540acc4328087cf
Sristy <126855796+Sristy-78@users.noreply.github.com>
2024-09-02 16:58:32 +0530

    Time: 99 ms (43.59%), Space: 76.8 MB (59.37%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -3,16 +3,12 @@ public:
     int chalkReplacer(vector<int>& chalk, int k) {
       long sum = 0;
         for (int& c : chalk) sum += c;
-        
         int chalkLeft = k % sum; // chalks for the last round
- 
         int n = chalk.size();
-        
         for (int i = 0; i < n; ++i) {
             if (chalk[i] > chalkLeft) return i;
             chalkLeft -= chalk[i];
         }
-        
         return -1;  
     }
 };
