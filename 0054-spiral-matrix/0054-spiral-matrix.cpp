96bfa30d1d0d1035f0a8074c414acba372b2b951
Sristy <126855796+Sristy-78@users.noreply.github.com>
2024-09-10 03:23:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (7.14%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -9,20 +9,24 @@ public:
       int colbegin=0;
       int colend=matrix[0].size()-1;
       while(rowbegin<=rowend && colbegin<=colend){
+        // this is for traversing from left to right
       for(int i=colbegin ; i<=colend;i++){
         result.push_back(matrix[rowbegin][i]);
       }
       rowbegin++;
+      // this is for traversing from top to bottom
       for(int i=rowbegin;i<=rowend;i++){
         result.push_back(matrix[i][colend]);
       }
       colend--;
+      // travering from right to left
       if(rowbegin<=rowend){
         for(int i=colend;i>=colbegin;i--){
             result.push_back(matrix[rowend][i]);
         }
       }
       rowend--;
+      // traversing from down to top
       if(colbegin<=colend){
         for(int j=rowend; j>=rowbegin; j--){
             result.push_back(matrix[j][colbegin]);
