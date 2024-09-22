7719a1ab1f386ab49fc3787b5297b5700088e263
Sristy <126855796+Sristy-78@users.noreply.github.com>
2024-09-23 05:09:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (57.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,17 +2,6 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
-        // for(auto it: s) {
-        //     if(it=='(' || it=='{' || it == '[') st.push(it); 
-        //     else {
-        //         if(st.size() == 0) return false; 
-        //         char ch = st.top(); 
-        //         st.pop(); 
-        //         if((it == ')' and ch == '(') or  (it == ']' and ch == '[') or (it == '}' and ch == '{')) continue;
-        //         else return false;
-        //     }
-        // }
-        // return st.empty(); 
       for(auto it:s){
         if(it=='(' || it=='{' || it=='[')
             st.push(it);
