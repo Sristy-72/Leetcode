6ac2f2037aab65044507ba94eed68a88ccad863e
Sristy <126855796+Sristy-78@users.noreply.github.com>
2024-10-02 20:44:02 +0530

    Time: 112 ms (16.93%), Space: 50 MB (15.61%) - LeetHub

---
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+         unordered_map<int, int> mp; //number -> rank
+        set<int> st(begin(arr), end(arr)); //ordered set keeps them in sorted order
+
+        int rank = 1;
+        for(auto &num : st) {
+            mp[num] = rank;
+            rank++;
+        }
+
+        for(int i = 0; i < arr.size(); i++) {
+            arr[i] = mp[arr[i]];
+        }
+
+        return arr;
+    }
+};
