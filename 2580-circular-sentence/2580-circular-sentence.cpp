3c0fc6c83f20860facef2dbd3be82f6e738a373c
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-11-02 20:11:18 +0530

    Time: 0 ms (100%), Space: 8.2 MB (83.91%) - LeetHub

---
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+       int n = sentence.length();
+        for (int i = 0; i < n; i++) {
+            if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1])
+                return false;
+        }
+        return sentence[0] == sentence[n - 1];    
+    }
+};
