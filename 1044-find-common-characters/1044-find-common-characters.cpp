ea0d9dd58a5484ac0d1efcd3c7b5fa9efc59a695
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-11-03 19:18:23 +0530

    Time: 0 ms (100%), Space: 11.7 MB (76.9%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.cpp
+++ 1044-find-common-characters/1044-find-common-characters.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
-      vector<string> commonChars(vector<string>& A) {
-        vector<map<char, int>> charCounts;
-        
-        //build maps for strings in A
-        for(string s : A){
-            map<char, int> charCount;
-            for(char c : s){
-                if(charCount.find(c)==charCount.end()){
-                    charCount[c] = 1;
-                }else{
-                    charCount[c] += 1;
-                }
-            }
-            charCounts.push_back(charCount);
+    vector<string> commonChars(vector<string>& words) {
+        vector<int> minFreq(26, INT_MAX);
+    for (const string& word : words) {
+        vector<int> freq(26, 0);
+        for (char c : word) {
+            freq[c - 'a']++;
         }
-        
-        map<char, int> charCount0 = charCounts[0];
-        for(int i = 1; i < charCounts.size(); i++){
-            //compare charCount0 and charCounts[i]
-            // cout << i << "th" << endl;
-            for(map<char, int>::iterator it=charCount0.begin(); it!=charCount0.end(); it++){
-                char c = it->first;
-                if(charCounts[i].find(c)==charCounts[i].end()){
-                    charCount0[c] = 0;
-                }else{
-                    charCount0[c] = min(charCount0[c], charCounts[i][c]);
-                }
-                // cout << c << ": " << charCount0[c] << ", ";
-            }
-            // cout << endl;
+        for (int i = 0; i < 26; i++) {
+            minFreq[i] = min(minFreq[i], freq[i]);
         }
-         vector<string> ans;
-        for(map<char, int>::iterator it=charCount0.begin(); it!=charCount0.end(); it++){
-            while(it->second>0){
-                ans.push_back(string(1,it->first));
-                it->second-=1;
-            }
+    }
+    vector<string> result;
+    for (int i = 0; i < 26; i++) {
+        while (minFreq[i] > 0) {
+            result.push_back(string(1, i + 'a'));
+            minFreq[i]--;
         }
-        
-        return ans;   
+    }
+
+    return result;  
     }
 };
