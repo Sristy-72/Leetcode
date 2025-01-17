29ea550439b6d98739ee2f1499405e8f95b16ad2
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-01-17 16:20:21 +0530

    Time: 0 ms (100%), Space: 10.9 MB (5.55%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+int solve(vector<int>&nums, int ind,vector<int>&dp){
+
+    // 1st :- recursion method (TLE)
+    // if(ind==0) return nums[ind];
+    // if(ind<0)return 0;
+    // int pick= nums[ind]+solve(nums,ind-2);
+    // int nonpick= 0+ solve(nums,ind-1);
+    // return max(pick, nonpick);
+
+    // memoization approach(memory limit exceeded)
+   
+    if(ind==0) return nums[ind];
+    if(ind<0) return 0;
+    if(dp[ind]!=-1) return dp[ind];
+    int pick=nums[ind]+solve(nums,ind-2, dp);
+    int nonpick=0+solve(nums,ind-1,dp);
+    return dp[ind]= max(pick, nonpick);
+
+    // tabulation method
+    // if(ind==0) return nums[0];
+    //  if(ind<0) return 0;
+    //  for(int i=1;i<nums.size();i++){
+    //     int curr=nums[]
+     }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+      vector<int>dp(n,-1);
+       return solve(nums,nums.size()-1, dp); 
+    }
+};
