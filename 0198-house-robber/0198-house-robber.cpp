1a01268968ceda00953ad12ee8e529ebf051f089
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-01-17 17:00:43 +0530

    Time: 0 ms (100%), Space: 10.8 MB (19.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-int solve(vector<int>&nums, int ind,vector<int>&dp){
+int solve(vector<int>&nums, int n,vector<int>&dp){
 
     // 1st :- recursion method (TLE)
     // if(ind==0) return nums[ind];
@@ -11,22 +11,30 @@ int solve(vector<int>&nums, int ind,vector<int>&dp){
 
     // memoization approach(memory limit exceeded)
    
-    if(ind==0) return nums[ind];
-    if(ind<0) return 0;
-    if(dp[ind]!=-1) return dp[ind];
-    int pick=nums[ind]+solve(nums,ind-2, dp);
-    int nonpick=0+solve(nums,ind-1,dp);
-    return dp[ind]= max(pick, nonpick);
+    // if(ind==0) return nums[ind];
+    // if(ind<0) return 0;
+    // if(dp[ind]!=-1) return dp[ind];
+    // int pick=nums[ind]+solve(nums,ind-2, dp);
+    // int nonpick=0+solve(nums,ind-1,dp);
+    // return dp[ind]= max(pick, nonpick);
 
     // tabulation method
-    // if(ind==0) return nums[0];
-    //  if(ind<0) return 0;
-    //  for(int i=1;i<nums.size();i++){
-    //     int curr=nums[]
+     dp[0] = nums[0];
+     for(int i=1;i<nums.size();i++){
+         int pick =nums[i];
+        if(i>1)
+      
+       pick += dp[i-2];
+       int nonpick= dp[i-1];
+      
+         dp[i]= max(pick,nonpick);
+     }
+     return dp[n-1];
+
      }
     int rob(vector<int>& nums) {
         int n=nums.size();
       vector<int>dp(n,-1);
-       return solve(nums,nums.size()-1, dp); 
+       return solve(nums,n, dp); 
     }
 };
