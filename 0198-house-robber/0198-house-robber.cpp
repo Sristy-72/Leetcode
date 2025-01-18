d1f1b04505b98d7333440af4951a4ce7b861adfb
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-01-18 06:51:19 +0530

    Time: 0 ms (100%), Space: 10.8 MB (19.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-int solve(vector<int>&nums, int ind,vector<int>&dp){
+int solve(vector<int>&nums, int n,vector<int>&dp){
 
-//    if(ind==0) return nums[ind];
-//    if(ind<0) return 0;
+//    if(n==0) return nums[n];
+//    if(n<0) return 0;
+   
 //       int pick = nums[ind] + solve(nums,ind-2);
 //       int nonpick= 0+ solve(nums, ind-1);
 //       return max(pick,nonpick);
    
+    
+    // memoization
+   
+//    if(dp[ind]!= -1) return dp[ind];
+//    int pick = nums[ind] + solve(nums, ind-2, dp);
+//    int nonpick = 0 + solve(nums, ind-1, dp);
+//    return dp[ind]= max(pick, nonpick);
 
-   if(ind==0) return nums[ind];
-   if(ind<0) return 0;
-   if(dp[ind]!= -1) return dp[ind];
-   int pick = nums[ind] + solve(nums, ind-2, dp);
-   int nonpick = 0 + solve(nums, ind-1, dp);
-   return dp[ind]= max(pick, nonpick);
-
+     // tabulation
+     dp[0]=nums[0];
+    
+     for(int i=1;i<n;i++){
+         int pick = nums[i];
+        if(i>1)
+            pick+= dp[i-2];
+            int nonpick = dp[i-1];
+           dp[i]=max(pick,nonpick);
+     }
+      return dp[n-1];
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-      vector<int>dp(n,-1);
-       return solve(nums,n-1,dp); 
+      vector<int>dp(n,-1);//Declare a dp[] array of size n+1 and initialize it to -1
+       return solve(nums,n,dp); 
     }
 };
