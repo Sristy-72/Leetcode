6a10fd9701fd581972ef4c72662fb196837b2896
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-03-16 00:56:20 +0530

    Time: 0 ms (100%), Space: 17.6 MB (50.77%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -11,7 +11,13 @@ public:
 
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n + 1, -1);
-        return min(ways(dp, n - 1, cost), ways(dp, n - 2, cost));
+        vector<int> dp(n , 0);
+        //return min(ways(dp, n - 1, cost), ways(dp, n - 2, cost));
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<n;i++){
+            dp[i] = min(dp[i-1],dp[i-2])+cost[i];
+        }
+        return min(dp[n-1],dp[n-2]);
     }
 };
