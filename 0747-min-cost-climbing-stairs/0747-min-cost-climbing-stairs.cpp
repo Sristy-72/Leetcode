ce8dba427bb0b36d351792c80a040af4e8ce2722
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-03-16 12:18:17 +0530

    Time: 0 ms (100%), Space: 17.7 MB (29.5%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -22,13 +22,13 @@ public:
 
 
         // SPACE OPTIMIZATION
-        int prev1=cost[1];
-        int prev2= cost[0];
-        for(int i=2;i<n;i++){
-            int curr= min( prev1,prev2) +cost[i];
-            prev2=prev1;
-            prev1=curr;
-                    }
-                    return min(prev1,prev2);
+          int prev2=cost[0];
+       int prev1=cost[1];
+       for(int i=2;i<n;i++){
+        int curr=min(prev1,prev2)+cost[i];
+        prev2=prev1;
+        prev1=curr;
+       }
+       return min(prev1,prev2);
     }
 };
