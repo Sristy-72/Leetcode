ff8d87081688c2cb026120902cab0ca1fc0fcd6f
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-03-17 22:57:57 +0530

    Time: 12 ms (5.86%), Space: 16.7 MB (84.67%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,47 +1,22 @@
-// class Solution {
-// public:
-//     bool divideArray(vector<int>& nums) {
-//         int n= nums.size();
-//         if(n%2!=0) return false;
-//         vector<bool>used(n,false);
-
-//        for(int i=0;i<n;i++){
-//         if(used[i])continue;
-//         bool foundpair= false;
-//         for(int j=1;j<n;j++){
-//             if( !used[j] && nums[i]==nums[j]){
-//             used[j]=true;
-//             foundpair=true;
-//             }
-//         }
-//         if(!foundpair) return false;
-//        } 
-//        return true;
-//     }
-// };
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        int n = nums.size();
-        if (n % 2 != 0) return false; // If odd length, cannot be divided into pairs
+        int n= nums.size();
+        if(n%2!=0) return false;
+        vector<bool>used(n,false);
 
-        vector<bool> used(n, false); // Track used elements
-
-        for (int i = 0; i < n; i++) {
-            if (used[i]) continue; // Skip if already paired
-            bool foundPair = false;
-
-            for (int j = i + 1; j < n; j++) {
-                if (!used[j] && nums[i] == nums[j]) { // Find an unpaired matching element
-                    used[j] = true;
-                    foundPair = true;
-                    break;
-                }
+       for(int i=0;i<n;i++){
+        if(used[i])continue;
+        bool foundpair= false;
+        for(int j=i+1;j<n;j++){
+            if( !used[j] && nums[i]==nums[j]){
+            used[j]=true;
+            foundpair=true;
+            break;
             }
-
-            if (!foundPair) return false; // If no pair is found, return false
         }
-
-        return true;
+        if(!foundpair) return false;
+       } 
+       return true;
     }
 };
