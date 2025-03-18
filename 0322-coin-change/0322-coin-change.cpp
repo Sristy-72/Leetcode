a5462ed2b73fb0a283ba9f4b3e8763498f539faf
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-03-18 20:35:19 +0530

    Time: 43 ms (39.71%), Space: 49.3 MB (27.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-int solve(int ind, int target, vector<int>&coins, vector<vector<int>>&dp){
- if(ind==0) {
-    if(target%coins[ind]==0){
-    return target/coins[0]; 
- }
- else
- return 1e9;
- }
- if(dp[ind][target]!=-1) return dp[ind][target];
-int take= 1e9;
-int notTake= 0+solve(ind-1,target,coins,dp);
-if(target>=coins[ind]){
-    take= 1+ solve(ind,target-coins[ind],coins, dp);
-}
-return dp[ind][target]= min( take, notTake);
+int solve(vector<vector<int>>&dp,vector<int>&arr, int ind, int target){
+  if(ind==0){
+    if(target%arr[0]==0){
+        return target/arr[0];
+    }
+    else
+    return 1e9;
+  }
+  if(dp[ind][target]!=-1) return dp[ind][target];
+  int notTake= solve(dp,arr, ind-1, target) + 0;
+  int take= 1e9;
+  if(arr[ind]<=target){
+    take= 1+solve(dp,arr, ind,target-arr[ind]);
+  }
+  return dp[ind][target]= min(take,notTake);
 }
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=solve(n-1,amount,coins,dp);
-    if(ans>=1e9)
-    return -1;
-    return ans;
+        int n= coins.size();
+      vector<vector<int>>dp(n, vector<int>(amount+1,-1) );
+       int ans=solve(dp,coins,n-1,amount);
+       if(ans>=1e9)
+       return -1;
+       return ans;
     }
 };
