528898b08585095034c1cd393a877a2c75b5bedb
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-03-19 20:42:00 +0530

    Time: 49 ms (27.49%), Space: 48.9 MB (33.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -18,8 +18,29 @@ int solve(vector<vector<int>>&dp,vector<int>&arr, int ind, int target){
 }
     int coinChange(vector<int>& coins, int amount) {
         int n= coins.size();
-      vector<vector<int>>dp(n, vector<int>(amount+1,-1) );
-       int ans=solve(dp,coins,n-1,amount);
+    //   vector<vector<int>>dp(n, vector<int>(amount+1,-1) );
+
+    //    int ans=solve(dp,coins,n-1,amount);
+    
+     vector<vector<int>>dp(n, vector<int>(amount+1,1e9) );
+    for(int i=0;i<=amount;i++){
+        if(i%coins[0]==0){
+             dp[0][i]= i/coins[0];
+        }
+        else
+        dp[0][amount]= 1e9;
+    }
+    for(int i=1;i<n;i++){
+        for(int j=0;j<=amount;j++){
+            int notTake=dp[i-1][j];
+            int take= 1e9;
+            if(coins[i]<=j){
+                take=1+ dp[i][j-coins[i]];
+            }
+            dp[i][j]= min(take,notTake);
+        }
+    }
+    int ans= dp[n-1][amount];
        if(ans>=1e9)
        return -1;
        return ans;
