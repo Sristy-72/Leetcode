85469a4699044a81e8b32203f4c5ce26e115aa09
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-04-02 19:38:05 +0530

    Time: 137 ms (37.99%), Space: 16.9 MB (69.2%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,30 +1,59 @@
+// class Solution {
+// public:
+// bool solve(vector<vector<int>>&dp,vector<int>&nums,int ind, int target){
+//     if(ind==0) return nums[0]==target;
+//     if(target==0) return true;
+//     if(dp[ind][target]!=-1) return dp[ind][target];
+//     bool notTaken= solve(dp, nums, ind-1,target);
+//     bool taken= false;
+//     if(nums[ind]<=target){
+//         taken= solve(dp,nums, ind-1, target-nums[ind]);
+//     }
+//     return dp[ind][target]= taken|| notTaken;
+// }
+//     bool canPartition(vector<int>& nums) {
+//         int n= nums.size();
+//          int sum=0;
+//         for(int i=0;i<n;i++){
+//             sum= sum+nums[i];
+//         }
+//         if(sum%2==1) {
+//             return false;
+//         }
+//         else{
+//              int target=sum/2;
+//           vector<vector<int>>dp(n, vector<int>(target+1,-1));
+//         return solve(dp,nums,n-1,target);
+//         }
+//         return false;
+//     }
+// };
 class Solution {
 public:
-bool solve(vector<vector<int>>&dp,vector<int>&nums,int ind, int target){
-    if(ind==0) return nums[0]==target;
-    if(target==0) return true;
-    if(dp[ind][target]!=-1) return dp[ind][target];
-    bool notTaken= solve(dp, nums, ind-1,target);
-    bool taken= false;
-    if(nums[ind]<=target){
-        taken= solve(dp,nums, ind-1, target-nums[ind]);
-    }
-    return dp[ind][target]= taken|| notTaken;
-}
     bool canPartition(vector<int>& nums) {
-        int n= nums.size();
-         int sum=0;
-        for(int i=0;i<n;i++){
-            sum= sum+nums[i];
+        int n = nums.size();
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        
+        if (sum % 2 == 1) return false;
+        
+        int target = sum / 2;
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
         }
-        if(sum%2==1) {
-            return false;
+        if (nums[0] <= target) dp[0][nums[0]] = true;
+        
+        for (int ind = 1; ind < n; ind++) {
+            for (int t = 1; t <= target; t++) {
+                bool notTaken = dp[ind - 1][t];
+                bool taken = false;
+                if (nums[ind] <= t) {
+                    taken = dp[ind - 1][t - nums[ind]];
+                }
+                dp[ind][t] = taken || notTaken;
+            }
         }
-        else{
-             int target=sum/2;
-          vector<vector<int>>dp(n, vector<int>(target+1,-1));
-        return solve(dp,nums,n-1,target);
-        }
-        return false;
+        
+        return dp[n - 1][target];
     }
 };
