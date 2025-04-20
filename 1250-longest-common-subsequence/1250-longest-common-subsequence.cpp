a6f03d53be53251255924b494ff4e97aa083008c
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-04-20 13:11:51 +0530

    Time: 47 ms (11.82%), Space: 27.6 MB (14.62%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -5,7 +5,7 @@ int solve(string &text1, string &text2, int i, int j , vector<vector<int>>&dp){
     int take=0;
     if(dp[i][j]!=-1) return dp[i][j];
     if(text1[i]==text2[j]){
-      return   dp[i][j]=  take=1+solve(text1, text2, i-1, j-1,dp);
+      return   dp[i][j]=1+solve(text1, text2, i-1, j-1,dp);
     }
     else{
        return dp[i][j]= max(solve(text1,text2,i-1,j,dp),solve(text1,text2,i,j-1,dp));
