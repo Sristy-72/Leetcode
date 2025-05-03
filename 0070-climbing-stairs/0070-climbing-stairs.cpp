a9056f39761e6a3c6ea4b96a514fa40a42034af4
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-05-03 07:59:37 +0530

    Time: 2 ms (4.99%), Space: 8.6 MB (33.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
+int solve(int n, vector<int>&dp){
+    if(n==0) return 1;
+    if(n<0) return 0;
+    if( dp[n]!=-1) return dp[n];
+    dp[n] = solve(n-1,dp) + solve(n-2,dp);
+    return dp[n];
+}
     int climbStairs(int n) {
-    //   if(n<=1) return 1;
-    //  return climbStairs(n-1)+climbStairs(n-2);  -----> time limit exceeded
-
-
-    // tabulation approch
-    // vector<int>dp(n+1, -1);
-    // dp[0]=1;
-    // dp[1]=1;
-    // for(int i=2;i<=n;i++){
-    //     dp[i]=dp[i-1]+dp[i-2];
-    // }
-    // return dp[n];
-
-    // memorization
-    int prev2=1;
-    int prev=1;
-    for(int i=2;i<=n;i++){
-        int curr= prev2+prev;
-        prev2=prev;
-        prev=curr;
-    }
-    return prev;
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
     }
 };
