6a4044a414b8185c491af99523d36c303de2682a
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-05-04 08:15:56 +0530

    Time: 23 ms (78.63%), Space: 27.5 MB (50.83%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -15,26 +15,26 @@ int solve(string &s1, string &s2 , int i ,int j, vector<vector<int>>&dp){
     int longestCommonSubsequence(string text1, string text2) {
          int n= text1.size();
          int m= text2.size();
-
-          vector<vector<int>>dp(n,vector<int>(m,-1));
-          return solve(text1, text2,n-1,m-1,dp);
-        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        // for(int i=0;i<=n;i++){
-        //     dp[i][0]=0;
-        // }
-        // for(int j=0;j<=m;j++){
-        //     dp[0][j]=0;
-        // }
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=m;j++){
-        //       if(text1[i-1]==text2[j-1]){
-        //         dp[i][j]=1+ dp[i-1][j-1];
-        //       }
-        //       else{
-        //         dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
-        //       }
-        //     }
-        // }
-        // return dp[n][m];
+        //  vector<vector<int>>dp(n,vector<int>(m,-1));
+        //  return solve(text1, text2,n-1,m-1,dp);
+       vector<vector<int>>dp(n+1, vector<int>(m+1,-1));
+       for(int i=0;i<=n;i++){
+        dp[i][0]=0;
+       }
+       for(int j=0;j<=m ;j++){
+        dp[0][j]=0;
+       }
+       for(int i=1;i<=n;i++){
+        for(int j=1; j<=m;j++){
+            if(text1[i-1]==text2[j-1]){
+                dp[i][j]= 1+ dp[i-1][j-1];
+            }
+            else {
+                dp[i][j]= max(dp[i-1][j], dp[i][j-1]);
+            }
+           
+        }
+       }
+        return dp[n][m];
     }
 };
