4dbe7d36532ba7e38d67a872ac12781f62cd4cba
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-07-07 12:00:01 +0530

    Time: 40 ms (73.47%), Space: 45.4 MB (57.77%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,30 +3,33 @@ public:
     int subarraySum(vector<int>& arr, int k) {
         int n=arr.size();
         int cnt=0;
-    //     for(int i=0;i<n;i++){
-    //         sum= sum+nums[i];
-    //         if(sum==k){
+   // BRUTE FORCE APPROACH
+    // for(int i=0;i<n;i++){
+    //      int sum=0;
+    //     for(int j=i;j<n;j++){
+    //         sum= sum+arr[j];
+    //         if(sum==k) {
     //             cnt++;
     //         }
-        
-    //     int newsum= sum;
-    //     newsum=newsum- nums[i]+ nums[i-k];
-    //     if(newsum==k){
-    //         cnt=cnt+1;
-    //     }
-    //     }
-    // return cnt;
-
-    for(int i=0;i<n;i++){
-         int sum=0;
-        for(int j=i;j<n;j++){
-            sum= sum+arr[j];
-            if(sum==k) {
-                cnt++;
-            }
            
-        }
+    //     }
+    // }
+    // return cnt;
+  int sum=0;
+unordered_map<int, int> mp;
+mp[0]=1;
+for(int i=0;i<n;i++){
+    sum= sum + arr[i];
+    // if(sum==k){
+    //     cnt++;
+    // }
+    if(mp.find(sum-k)!=mp.end()){
+        cnt= cnt+ mp[sum-k];
     }
-    return cnt;
+        mp[sum]++;
+    
+   
+}
+return cnt;
     }
 };
