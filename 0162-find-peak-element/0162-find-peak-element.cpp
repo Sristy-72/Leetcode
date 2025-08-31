b41c397a6a36310f3634d8d5f44b85a84e50e876
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-08-31 18:29:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& arr) {
-     int n=arr.size();
-          int s=1, e= n-2;
-         // Edge cases:
-    if (n == 1) return 0;
-    if (arr[0] > arr[1]) return 0;
-    if (arr[n - 1] > arr[n - 2]) return n - 1;
-
-        while(s<=e){
-            //1 6 3  2 1 -> if we take this condition then it will try to comapare 1 with less than
-            // 1 which is not present in the array so it will give error so we will take low as  //   and high as n-2
-            int mid=(e+s)/2;
-            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){// peak element
-                return mid; // if we find that mid is the largest the we will retun the mid directly
-            }
-            else if(arr[mid]>arr[mid-1]){
-                s=mid+1;// we will go to right side
-            } else {
-                e = mid - 1; // else we will go to left side
-            }
+    int findPeakElement(vector<int>& nums) {
+        int n= nums.size();
+        int st=0;
+        int end= n-1;
+        int mid= (st+end)/2;
+        if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+          return mid;
+        } 
+        else if(nums[mid-1]>nums[mid]){
+          end= mid-1;
         }
-        return -1;
+        else{
+            st= mid+1;
+        }
+        mid= (st+end)/2;
+        return mid;
     }
 };
