228757cd259cbab1cc0fe4fffbab050ee8665aad
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-09-11 14:18:49 +0530

    Time: 4 ms (30.29%), Space: 21.4 MB (10.16%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+void bfs(vector<vector<int>>&adjls ,vector<int>&vis,int start ){
+   vis[start]=1;
+    queue<int>q;
+   q.push(start);
+   while(!q.empty()){
+    int node= q.front();
+    q.pop();
+    for(auto it:adjls[node]){
+        if(!vis[it]){
+            vis[it]=1;
+            q.push(it);
+        }
+    }
+   }
+  
+}
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n= isConnected.size();
+        vector<int>vis(n,0);
+       vector< vector<int>>adjls(n);
+        // converting adjacency matrix to adjacency list
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+               if(isConnected[i][j]==1 && i!=j){
+                adjls[i].push_back(j);
+                adjls[j].push_back(i);
+               }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+         if(!vis[i]){
+            cnt++;
+            bfs(adjls, vis,i);
+         }
+         
+        }
+
+      return cnt;
+       
+    }
+};
