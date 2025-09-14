cac9be4c0192d9d3b48625d23db914df2015e09f
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-09-14 18:58:12 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49.16%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -10,26 +10,46 @@ public:
     //    }
     //    } 
 
-    int cnt0=0;
-    int cnt1=0;
-    int cnt2=0;
-    for(int i=0;i<n;i++){
-        if(nums[i]==0){
-            cnt0++;
+    // int cnt0=0;
+    // int cnt1=0;
+    // int cnt2=0;
+    // for(int i=0;i<n;i++){
+    //     if(nums[i]==0){
+    //         cnt0++;
+    //     }
+    //     else if(nums[i]==1){
+    //         cnt1++;
+    //     }
+    //     else cnt2++;
+    // }
+    //     for(int i=0;i<cnt0;i++){
+    //         nums[i]=0;
+    //     } 
+    //     for(int i=cnt0;i<cnt1+cnt0;i++){
+    //         nums[i]=1;
+    //     }
+    //     for(int i=cnt0+cnt1;i<cnt2+cnt1+cnt0;i++){
+    //         nums[i]=2;
+    //     }
+
+    int low=0;
+    int high= n-1;
+
+    int mid=0;
+    while(mid<=high){
+        if(nums[mid]==0){
+            swap(nums[mid], nums[low]);
+            mid++;
+            low++;
         }
-        else if(nums[i]==1){
-            cnt1++;
+        else if(nums[mid]==1){
+            mid++;
         }
-        else cnt2++;
+        else{
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    
     }
-        for(int i=0;i<cnt0;i++){
-            nums[i]=0;
-        } 
-        for(int i=cnt0;i<cnt1+cnt0;i++){
-            nums[i]=1;
-        }
-        for(int i=cnt0+cnt1;i<cnt2+cnt1+cnt0;i++){
-            nums[i]=2;
-        }
     }
 };
