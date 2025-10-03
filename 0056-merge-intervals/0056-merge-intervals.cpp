3c023c2f805c2a563aa4012b86f983fcb4b236c4
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-10-03 21:37:14 +0530

    Time: 3 ms (89.44%), Space: 23.9 MB (31.21%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>>ans;
+        int n = intervals.size();
+       for(int i=0;i<n;i++){
+       if(ans.empty()){
+        ans.push_back(intervals[i]);
+       }
+       else{
+        vector<int>&v=ans.back();
+        int y=v[1];
+        if(intervals[i][0]<=v[1]){
+            v[1]= max(y, intervals[i][1]);
+
+        }
+        else{
+            ans.push_back(intervals[i]);
+        }
+       }
+       } 
+       return ans;
+    }
+};
