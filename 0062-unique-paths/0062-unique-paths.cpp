dbafb4f1a68f19e4076660e29f9b3225e8582eb1
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-11-10 11:23:33 +0530

    Time: 2 ms (19.21%), Space: 9.5 MB (19.72%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,46 +1,15 @@
-// class Solution {
-// public:
-// int solve(int m,int n,vector<vector<int>>&dp){
-//     if(m==0 && n==0) return 1;
-//     if(n<0 || m<0) return 0;
-//     if(dp[m][n] != -1) return dp[m][n];
-//     int up= solve(m-1,n,dp);
-//     int from= solve(m,n-1,dp);
-//     return dp[m][n]= up+from;
-// }
-//     int uniquePaths(int m, int n) {
-//         vector<vector<int>>dp(m,vector<int>(n,-1));
-//         return solve(m-1,n-1,dp);
-//     }
-// };
-
 class Solution {
-  public:
-   int uniquePaths(int m, int n) {
-    vector<vector<int>>dp(m,vector<int>(n,0));
-    dp[0][0]=1;
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-         if(i>0) dp[i][j]+=dp[i-1][j];
-         if(j>0)dp[i][j]+= dp[i][j-1];
+public:
+int solve(int i, int j, vector<vector<int>>&dp){
+    if(i==0 && j==0) return 1;
+    if(i<0 || j<0) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+    int down= solve(i-1, j, dp);
+    int right= solve(i, j-1, dp);
+    return dp[i][j]=down+right;
+}
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m, vector<int>(n,-1));
+        return solve(m-1,n-1, dp);
     }
-    }
-    return dp[m-1][n-1];
-    
-
-     
-    
-    // for(int col=0;col<n;col++){
-    // dp[0][col]=1;
-    // }
-    // for(int row=0;row<m;row++){
-    //     dp[row][0]=1;
-    // }
-    // for(int i=1;i<m;i++){
-    //     for(int j=1;j<n;j++){
-    //         dp[i][j]=dp[i-1][j]+ dp[i][j-1];
-    //     }
-    // }
-    // return dp[m-1][n-1];
-   }
 };
