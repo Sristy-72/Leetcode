777e184f254e564d55b3acc33e6bc4ec611dabe8
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-11-10 07:59:43 +0530

    Time: 0 ms (100%), Space: 8.4 MB (11.19%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -9,13 +9,13 @@ public:
         vector<int>dp(n+1);
         // return   solve(n, dp);
          if (n <= 1) return n;
-         dp[0]=0;
-         dp[1]=1;
+        int prev1= 1;
+        int prev2=0;
         for(int i=2;i<=n;i++){
-            dp[i]= dp[i-1]+dp[i-2];
+            int curr= prev1+prev2;
+            prev2= prev1;
+            prev1=curr;
         }
-        return dp[n];
-        // if(n<=1) return n;
-        // return fib(n-1)+fib(n-2);
+        return prev1;
     }
 };
