5faf14b82686aaa0bc679149cc1a903afdba2609
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-11-13 20:19:27 +0530

    Time: 2 ms (15.44%), Space: 17.1 MB (28.66%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int>ans;
+        int n= nums.size();
+        for(int i=0;i<2;i++){
+            for(int i=0;i<n;i++){
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
