62a7b0b28bb7d443b51b75d3f247cd39ceaa9856
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-11-14 16:27:53 +0530

    Time: 0 ms (100%), Space: 8.6 MB (6.79%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -9,6 +9,18 @@ int solve(int ind, vector<int>&dp){
 }
     int tribonacci(int n) {
       vector<int>dp(n+1,-1);
-     return solve(n, dp);
+     //return solve(n, dp);
+     int prev3=0;
+     int prev2=1;
+     int prev1=1;
+     if(n==0) return 0;
+     for(int i=3;i<=n;i++){
+        int curr= prev1+prev2+prev3;
+        prev3= prev2;
+        prev2=prev1;
+        prev1=curr;
+
+     }
+     return prev1;
     }
 };
