8d9925de4a6ceb478ef0a35cc5a5aaf114c16d29
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-11-23 07:47:45 +0530

    Time: 0 ms (100%), Space: 7.8 MB (84.55%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+       int prev3=0;
+     int prev2=1;
+     int prev1=1;
+     if(n==0) return 0;
+     for(int i=3;i<=n;i++){
+        int curr= prev1+prev2+prev3;
+        prev3= prev2;
+        prev2=prev1;
+        prev1=curr;
+
+     }
+     return prev1;   
+    }
+};
