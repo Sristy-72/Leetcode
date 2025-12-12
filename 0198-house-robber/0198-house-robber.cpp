a8fef8898774f093b91e593ab8415aeea17785e7
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-12-12 14:43:55 +0530

    Time: 0 ms (100%), Space: 10.8 MB (21.51%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,6 +12,17 @@ int solve(vector<int>&nums, int ind, vector<int>&dp){
     int rob(vector<int>& nums) {
         int n= nums.size();
         vector<int>dp(n,-1);
-         return solve(nums, n-1, dp);
+        // return solve(nums, n-1, dp);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int adj1= nums[i];
+            int adj2= nums[0];
+            if(i>1){
+                adj1= adj1+dp[i-2];
+                adj2= dp[i-1];
+            }
+            dp[i]= max(adj1, adj2);
+        }
+        return dp[n-1];
     }
 };
