ee6127758ada2a386ecdc2247de74767bbd4d79c
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-12-19 23:09:47 +0530

    Time: 0 ms (100%), Space: 52.3 MB (65.38%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n= nums.size();
+         int n= nums.size();
         int lastindex=n-1;
        for(int i=n-2;i>=0;i--){
         int jump= nums[i]+i;
