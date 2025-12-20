44be824e22a2526d6e785746535531c77cc943b1
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-12-20 13:26:02 +0530

    Time: 0 ms (100%), Space: 10.7 MB (44.4%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,46 +1,17 @@
 class Solution {
 public:
-int solve(vector<int>&nums, int ind, vector<int>&dp){
+int solve(vector<int>&nums, int ind,vector<int>&dp){
     if(ind==0) return nums[0];
     if(ind<0) return 0;
-    if(dp[ind]!=-1) return dp[ind];
+      if(dp[ind]!=-1) return dp[ind];
+       int  pick=nums[ind]+solve(nums,ind-2,dp);
+        int  not_pick=solve(nums, ind-1,dp);
+    return dp[ind]= max(pick, not_pick);
 
-     int adj1= nums[ind]+ solve(nums,ind-2,dp);
-     int adj2= 0+ solve(nums, ind-1,dp);
-     return dp[ind]=max(adj1, adj2);
 }
     int rob(vector<int>& nums) {
         int n= nums.size();
-        vector<int>dp(n,-1);
-        // return solve(nums, n-1, dp);
-        // dp[0]=nums[0];
-        // for(int i=1;i<n;i++){
-        //     int adj1= nums[i];
-        //     int adj2= nums[0];
-        //     if(i>1){
-        //         adj1= adj1+dp[i-2];
-        //         adj2= dp[i-1];
-        //     }
-        //     dp[i]= max(adj1, adj2);
-        // }
-        // return dp[n-1];
-
-        // space optimization
-
-        if(n==1) return nums[0];
-       int prev2=nums[0];
-       int prev1=max(nums[0], nums[1]);
-       int curr= INT_MIN;
-       for(int i=1;i<n;i++){
-        int take=nums[i];
-        if(i>1){
-            take= take+prev2;
-            int not_take= prev1;
-             curr= max(take, not_take);
-            prev2=prev1;
-            prev1= curr;
-        }
-       }
-     return prev1;
+          vector<int>dp(n,-1);
+          return solve(nums,n-1,dp);
     }
 };
