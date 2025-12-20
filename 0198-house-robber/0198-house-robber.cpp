c32e47144ab919665f9414901726aa7e9d3e8927
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-12-20 14:07:28 +0530

    Time: 0 ms (100%), Space: 10.6 MB (44.4%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,7 +11,17 @@ int solve(vector<int>&nums, int ind,vector<int>&dp){
 }
     int rob(vector<int>& nums) {
         int n= nums.size();
-          vector<int>dp(n,-1);
-          return solve(nums,n-1,dp);
+          vector<int>dp(n);
+          //return solve(nums,n-1,dp);
+         if(n==0) return 0;
+         if(n==1) return nums[0];
+           dp[0]=nums[0];
+           dp[1]= max(nums[0], nums[1]);
+          for(int i=2;i<n;i++){
+            int pick= nums[i]+dp[i-2];
+            int not_pick= dp[i-1];
+            dp[i]=max(pick, not_pick);
+          }
+          return dp[n-1];
     }
 };
