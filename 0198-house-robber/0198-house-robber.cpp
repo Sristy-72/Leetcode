d7535b0d9cd3551362a47bdce97c2c3cfbe397b2
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-12-20 15:27:11 +0530

    Time: 3 ms (5.88%), Space: 10.7 MB (21.38%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -15,13 +15,24 @@ int solve(vector<int>&nums, int ind,vector<int>&dp){
           //return solve(nums,n-1,dp);
          if(n==0) return 0;
          if(n==1) return nums[0];
-           dp[0]=nums[0];
-           dp[1]= max(nums[0], nums[1]);
-          for(int i=2;i<n;i++){
-            int pick= nums[i]+dp[i-2];
-            int not_pick= dp[i-1];
-            dp[i]=max(pick, not_pick);
-          }
-          return dp[n-1];
+        //    dp[0]=nums[0];
+        //    dp[1]= max(nums[0], nums[1]);
+        //   for(int i=2;i<n;i++){
+        //     int pick= nums[i]+dp[i-2];
+        //     int not_pick= dp[i-1];
+        //     dp[i]=max(pick, not_pick);
+        //   }
+        //   return dp[n-1];
+
+        int prev2= nums[0];
+        int prev = max(nums[0], nums[1]);
+        for(int i=2;i<n;i++){
+            int take= nums[i]+prev2;
+            int not_take=prev;
+            int curr= max(take, not_take);
+            prev2= prev;
+            prev= curr;
+        }
+        return prev;
     }
 };
