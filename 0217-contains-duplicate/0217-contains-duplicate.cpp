793c571457500abc2c560c1978000dabf176f84c
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-12-30 19:00:41 +0530

    Time: 51 ms (54.85%), Space: 90.9 MB (44.31%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
-        for (int num : nums) {
-            if (seen.count(num)) {
-                return true;
-            }
-            seen.insert(num);
+        unordered_map<int, int>mp;
+        int n= nums.size();
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+            if(mp[nums[i]]>1) return true;
         }
         return false;
     }
