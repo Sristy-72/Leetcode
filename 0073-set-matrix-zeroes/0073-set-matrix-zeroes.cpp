3274ba64fa548be5a8598ba4438b3e4d2762fff7
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-12-31 13:22:45 +0530

    Time: 0 ms (100%), Space: 18.7 MB (49.83%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,52 +1,23 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m= matrix.size();//row size
-        int n= matrix[0].size();//col size
-        int x=1;
-        int y=1;
-         for(int j=0;j<m;j++){
-        if(matrix[j][0]==0){
-            x=0;
-        }
-       }
-       for(int i=0;i<n;i++){
-        if(matrix[0][i]==0){
-            y=0;
-        }
-       } 
-      
-       for(int i=1;i<m;i++){
-        for(int j=1;j<n;j++){
+        int m= matrix.size();
+        int n= matrix[0].size();
+       vector<int>dummyrow(m,-1);
+       vector<int>dummycol(n,-1);
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
             if(matrix[i][j]==0){
-                matrix[0][j]=0;
-                matrix[i][0]=0;
+                dummyrow[i]=1;
+                dummycol[j]=1;
             }
         }
        }
-
-       for(int j=1;j<n;j++){
-        if(matrix[0][j]==0){
-            for(int i=1;i<m;i++){
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(dummyrow[i]==1 || dummycol[j]==1){
                 matrix[i][j]=0;
             }
-        }
-       }
-       for(int j=1;j<m;j++){
-        if(matrix[j][0]==0){
-            for(int i=1;i<n;i++){
-                matrix[j][i]=0;
-            }
-        }
-       }
-       if(x==0){
-        for(int i=0;i<m;i++){
-            matrix[i][0]=0;
-        }
-       }
-       if(y==0){
-        for(int j=0;j<n;j++){
-            matrix[0][j]=0;
         }
        }
     }
