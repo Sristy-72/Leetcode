98084f076ec51ada6670a45634a81cdaa1ec12b6
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-01-07 19:46:12 +0530

    Time: 0 ms (100%), Space: 10.7 MB (49.17%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-void allcomb(vector<int>&nums, vector<vector<int>>&ans, vector<int>&ds, vector<int>&freq){
-    if(ds.size()==nums.size()){
-        ans.push_back(ds);
+void allComb(vector<int>&nums, vector<vector<int>>&ans, vector<int>&cont,vector<int>&freq){
+    if(cont.size()==nums.size()){
+        ans.push_back(cont);
     }
     for(int i=0;i<nums.size();i++){
         if(!freq[i]){
-            ds.push_back(nums[i]);
+            cont.push_back(nums[i]);
             freq[i]=1;
-            allcomb(nums, ans, ds, freq);
+            allComb(nums,ans, cont, freq);
             freq[i]=0;
-            ds.pop_back();
+            cont.pop_back();
         }
     }
-}
+} 
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
+         vector<vector<int>>ans;
         int n= nums.size();
-        vector<int>ds;
+        vector<int>cont;
        vector<int>freq(n);
-          allcomb(nums, ans, ds, freq);
+          allComb(nums, ans, cont, freq);
         return ans;
     }
 };
