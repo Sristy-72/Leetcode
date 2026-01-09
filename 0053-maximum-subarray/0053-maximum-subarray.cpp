8734de86439391ee71ce74cd5dfb52e8d93eb449
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-01-09 23:34:51 +0530

    Time: 4 ms (11.95%), Space: 71.9 MB (16.89%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,8 +5,8 @@ public:
     int maxi = nums[0];
 
     for(int i = 1; i < nums.size(); i++){
-        curr = max(nums[i], curr + nums[i]);
-        maxi = max(maxi, curr);
+        curr = max(nums[i], curr + nums[i]);// take or not-take  ----> we will take if adding that number increases the value and we will not take if adding that valu decreases that value
+        maxi = max(maxi, curr); // checking which subarray sum is maximum
     }
     return maxi;
     }
