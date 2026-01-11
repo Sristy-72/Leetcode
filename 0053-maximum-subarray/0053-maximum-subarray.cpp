cf5eabd23036bdad3c2d63ecea0198fa21fc97e2
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-01-11 17:15:12 +0530

    Time: 0 ms (100%), Space: 71.6 MB (80.08%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-   int curr = nums[0];
-    int maxi = nums[0];
-
-    for(int i = 1; i < nums.size(); i++){
-        curr = max(nums[i], curr + nums[i]);// take or not-take  ----> we will take if adding that number increases the value and we will not take if adding that valu decreases that value
-        maxi = max(maxi, curr); // checking which subarray sum is maximum
-    }
-    return maxi;
+        int n= nums.size();
+        int sum=0;
+        int maxi=nums[0];
+        for(int i=0;i<n;i++){
+            sum= sum+nums[i];
+            maxi= max(sum, maxi);
+            if(sum<0) sum=0;
+        }
+        return maxi;
     }
 };
