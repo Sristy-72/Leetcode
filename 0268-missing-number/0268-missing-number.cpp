c7e894bf2fac1913d55dc036e1de40d0edd21ef4
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-01-14 21:59:36 +0530

    Time: 0 ms (100%), Space: 21.8 MB (37.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        vector<int>ans;
         int n=nums.size();
-    //     int sum= (n*(n+1)/2);
-    //     //for(int i=0;i<n;i++){
-    //        // int sum1=sum+nums[i];
-    //    // }
-    //    return sum-accumulate(nums.begin(),nums.end(),0);
-    //    // accumulate is used to iterate over a sequence of elements
-    //    // accumulate(starting range, ending range,intial value)
-
-    int sum=0;
+      int sum=0;
     int sum2=0;
     int miss_number;
     for(int i=0;i<=n;i++){
@@ -20,6 +11,6 @@ public:
     for(int i=0;i<n;i++){
         sum2=sum2+nums[i];
     }
-    return sum-sum2;
+    return sum-sum2;   
     }
 };
