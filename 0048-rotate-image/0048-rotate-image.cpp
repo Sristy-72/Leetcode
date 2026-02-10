f082814fc97e891466493516075ac69b0ae9a6cb
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-02-10 19:11:56 +0530

    Time: 0 ms (100%), Space: 10.3 MB (35.93%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-       int m= matrix.size();
-       int n= matrix[0].size();
-       vector<vector<int>>ans(n,vector<int>(n,0));
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            ans[j][n-i-1]=matrix[i][j];
+        int m= matrix.size();
+        int n= matrix[0].size();
+        vector<vector<int>>arr(n,vector<int>(n));
+        for(int i=0;i<n;i++){
+            for(int j= 0;j<n;j++){
+              arr[i][j]=matrix[n-j-1][i];
+            }
         }
-       } 
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            matrix[i][j]=ans[i][j];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j]=arr[i][j];
+            }
         }
-       }
     }
 };
