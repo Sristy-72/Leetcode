a9b9c94f808617d4890189618e3b0335fc993a0b
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-02-25 23:49:47 +0530

    Time: 0 ms (100%), Space: 52.3 MB (49.91%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,7 +3,7 @@ public:
     bool canJump(vector<int>& nums) {
          int n= nums.size();
         int lastindex=n-1;
-       for(int i=n-2;i>=0;i--){
+         for(int i=n-2;i>=0;i--){
         int jump= nums[i]+i;
         if(jump>=lastindex){
             lastindex=i;
