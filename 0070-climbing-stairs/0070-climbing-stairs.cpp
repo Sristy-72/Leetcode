b03f5dc24903f70b2a4389abb86d1e4445e1c9e0
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-03-07 21:56:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
+int solve(vector<int>dp, int ind){
+   if(ind==0) return 1;
+   if(ind<0) return 0;
+   if(dp[ind]!=-1) return dp[ind];
+   return solve(dp, ind-1)+ solve(dp, ind-2); 
+}
     int climbStairs(int n) {
-    //    vector<int>dp(n+1, -1);
-       int prev1=1;
-       int prev2=1;
-       if(n<=1) return 1;
-       for(int i=2;i<=n;i++){
-        int curr= prev1+prev2;
-        prev2= prev1;
-        prev1= curr;
-       }
-       return prev1;
+        vector<int>dp(n+1,-1);
+       return solve(dp,n);
     }
 };
