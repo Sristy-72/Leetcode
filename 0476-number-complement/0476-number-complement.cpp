c602746bf38b718812d0980119a2b90ea7a4bbb8
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-03-11 19:47:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     int findComplement(int num) {
-        int mask = 1;
-        while (mask < num) {
-            mask = (mask << 1) | 1;
+        int result=0;
+        int power= 1;
+        if(num==0)return 1;
+        while(num>0){
+            int bit=num%2;
+            if(bit==0){
+                result= result+power;
+            }
+            power*= 2;
+            num=num/2;
         }
-        return num ^ mask;
+        return result;
     }
 };
