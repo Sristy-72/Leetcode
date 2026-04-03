bf66817e3df0edf5a995dfe59db14a5c77f59875
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-04-04 01:28:10 +0530

    Time: 0 ms (100%), Space: 10.3 MB (49.99%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        string word= s;
-        int n= s.length();
-        int start=0;
-        int end=n-1;
-        string vowels= "aeiouAEIOU";
-        while(start<end){
-            while(start<end && vowels.find(word[start])==string::npos){
-                start++;
-            }
-            while(start<end && vowels.find(word[end])==string::npos){
-                end--;
-            }
-            swap(word[start],word[end]);
-            start++;
-            end--;
-        }
-        return word;
+        int n=s.length();
+        int i=0;
+	int j= n-1;
+	while(i<j){
+		if((s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U') && (s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o' || s[j]=='u' || s[j]=='A' || s[j]=='E' || s[j]=='I' || s[j]=='O' || s[j]=='U') ){
+			swap(s[i],s[j]);
+            i++;
+            j--;
+		}
+		else if(s[i]=='a' || s[i]=='e' || s[i]=='i'|| s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
+			j--;
+		}
+		else if(s[j]=='a'  || s[j]=='e' || s[j]=='i' || s[j]=='o' || s[j]=='u' || s[j]=='A' || s[j]=='E' || s[j]=='I' || s[j]=='O' || s[j]=='U'){
+			i++;
+		}
+		else{
+			i++;
+			j--;
+		}
+	}
+    return s;
     }
 };
