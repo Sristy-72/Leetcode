25d24f66f8885fea699a03b95cbb45f659e84363
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-04-04 00:03:37 +0530

    Time: 0 ms (100%), Space: 10.5 MB (35.59%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        string word= s;
+        int n= s.length();
+        int start=0;
+        int end=n-1;
+        string vowels= "aeiouAEIOU";
+        while(start<end){
+            while(start<end && vowels.find(word[start])==string::npos){
+                start++;
+            }
+            while(start<end && vowels.find(word[end])==string::npos){
+                end--;
+            }
+            swap(word[start],word[end]);
+            start++;
+            end--;
+        }
+        return word;
+    }
+};
