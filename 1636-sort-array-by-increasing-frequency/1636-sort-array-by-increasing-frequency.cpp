7d88009d8902a334b1cf1f07b5de494869b70970
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-04-11 01:50:27 +0530

    Time: 0 ms (100%), Space: 14.9 MB (85.71%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -6,23 +6,33 @@ public:
         for(auto it:nums){
             mp[it]++;
         }
-        map<int , vector<int>>mpp;
-        for(auto it:mp){
-            mpp[it.second].push_back(it.first);
+         
+        sort(nums.begin(),nums.end(),[&](int a, int b){
+            if(mp[a]==mp[b]) return a>b;// if frequency is same then choose the bigger one
+            return mp[a]<mp[b];
 
-        }
-        vector<int>v;
-        for(auto it:mpp){
-            vector<int>y=it.second;
-            sort(y.begin(), y.end(),greater<int>());
-            for(int i=0;i<y.size();i++){
-                int x=it.first;
-                while(x>0){
-                    v.push_back(y[i]);
-                    x--;
-                }
-            }
-        }
-        return v;
+        });
+        return nums;
+
+
+
+        // map<int , vector<int>>mpp;
+        // for(auto it:mp){
+        //     mpp[it.second].push_back(it.first);
+
+        // }
+        // vector<int>v;
+        // for(auto it:mpp){
+        //     vector<int>y=it.second;
+        //     sort(y.begin(), y.end(),greater<int>());
+        //     for(int i=0;i<y.size();i++){
+        //         int x=it.first;
+        //         while(x>0){
+        //             v.push_back(y[i]);
+        //             x--;
+        //         }
+        //     }
+        // }
+        // return v;
     }
 };
