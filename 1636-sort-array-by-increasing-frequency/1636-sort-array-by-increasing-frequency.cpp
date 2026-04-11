b83a31acb29944d2d4dd35649ce01db780f53593
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-04-11 11:24:10 +0530

    Time: 3 ms (54.4%), Space: 15.6 MB (8.49%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        int n=nums.size();
         unordered_map<int,int>mp;
-        for(auto it:nums){
-            mp[it]++;
+    for(auto it:nums){
+     mp[it]++;
+    }
+    map<int, vector<int>>mpp;
+    for(auto it:mp){
+        mpp[it.second].push_back(it.first);
+    }
+    vector<int>v;
+    for(auto it:mpp){
+        vector<int>y= it.second;
+        sort(y.begin(),y.end(),greater<int>());
+        for(int i=0;i<y.size();i++){
+            int x=it.first;
+            while(x>0){
+                v.push_back(y[i]);
+                x--;
+            }
         }
-         
-        sort(nums.begin(),nums.end(),[&](int a, int b){
-            if(mp[a]==mp[b]) return a>b;// if frequency is same then choose the bigger one
-            return mp[a]<mp[b];
-
-        });
-        return nums;
-
-
-
-        // map<int , vector<int>>mpp;
-        // for(auto it:mp){
-        //     mpp[it.second].push_back(it.first);
-
-        // }
-        // vector<int>v;
-        // for(auto it:mpp){
-        //     vector<int>y=it.second;
-        //     sort(y.begin(), y.end(),greater<int>());
-        //     for(int i=0;i<y.size();i++){
-        //         int x=it.first;
-        //         while(x>0){
-        //             v.push_back(y[i]);
-        //             x--;
-        //         }
-        //     }
-        // }
-        // return v;
+    }
+    return v;
     }
 };
