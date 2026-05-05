94a19ac7d494806ef717eae3d6c45c87180481c2
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-05-05 20:32:34 +0530

    Time: 0 ms (100%), Space: 9.6 MB (62.33%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>arr;
-        
-        for(int i=0;i<numRows;i++){
+        arr.push_back({1});
+        for(int i=1;i<numRows;i++){
             vector<int>v(i+1,1);
             for(int j=1;j<i;j++){
-             v[j]=arr[i-1][j]+ arr[i-1][j-1];
+              v[j]=arr[i-1][j-1]+ arr[i-1][j];
+             
             }
             arr.push_back(v);
         }
