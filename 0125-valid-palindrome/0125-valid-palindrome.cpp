d637b7f5560aeb6a0d67ac7f870d97f7a5c59b56
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-06-15 14:03:12 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-     int n= s.size();
-       int start=0;
-       int end= n;
-       if(s==" ") return true;
-       while(start<end){
-        if(!isalnum(s[start])){
-            start++;
-        }
-        else if(!isalnum(s[end])){
-            end--;
-        }
-        else if(tolower(s[start])!=tolower(s[end])){
-        return false;
-        }
-        else{
-            start++;
-            end--;
-        }
-       }
-       return true;
+      int n= s.length();
+      string ans;
+      for(int i=0;i<n;i++){
+        if('A' <= s[i] && s[i]<='Z' || 'a' <= s[i] && s[i]<='z' || '0' <= s[i] && s[i]<='9' ) {
+            ans.push_back(tolower(s[i]));
+        } 
+      }
+      string ans2= ans;
+      reverse(ans2.begin(), ans2.end());
+      return ans2==ans;
     }
 };
