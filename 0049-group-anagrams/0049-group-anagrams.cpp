6d0b7197af120618a54193f4dd5a92ccf64ce4b0
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-07-14 20:49:41 +0530

    Time: 27 ms (23.14%), Space: 30.8 MB (17.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
-   
+  string generate(string temp){
+    vector<int>arr(26);
+    for(auto it:temp){
+        arr[it-'a']++;
+    }
+    string arranged_word="";
+    for(int i=0;i<26;i++){
+        if(arr[i]!=0) {
+            int freq= arr[i];
+          arranged_word+=string(freq, i+'a'); // used to form string from the freq of words 
+
+        }
+       
+    }
+     return arranged_word;
+  }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-       vector<vector<string>>ans;
-       int n= strs.size();
-       unordered_map<string, vector<string>>mp;
-       for(int i=0;i<n;i++){
-        string temp= strs[i];
-        sort(temp.begin(), temp.end());
-        mp[temp].push_back(strs[i]);
-       }
-       for(auto it:mp){
-        ans.push_back(it.second);
-       }
-       return ans;
+        int n= strs.size();
+        unordered_map<string, vector<string>>mp;
+        vector<vector<string>>ans;
+        for(int i=0;i<n;i++){
+            string temp=strs[i];
+            string word= generate(temp);
+              mp[word].push_back(temp);
+        }
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
     }
 };
