ca6b2fbb1488a584249db3cafd6d293ea8a9d5ef
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-07-26 17:25:36 +0530

    Time: 0 ms (100%), Space: 8.4 MB (87.81%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x<2)
-        return x;
-         int start= 0, end=x;
-         int mid;
-         int ans;
-         while(start<=end){
-              mid=start + (end- start)/2;
-            if(mid==x/mid){
-               return mid;
+        if(x==0 || x==1) return x;
+        int st=0;
+        int end=x;
+        int ans;
+        while(st<=end){
+            int mid= st + (end-st)/2;
+            if(mid==x/mid) return mid;
+            else if(mid<x/mid) {
+                st=mid+1;
+                ans= mid;
             }
-            else if(mid<x/mid){
-                start=mid+1;
-                ans=mid;
+            else {
+                end= mid-1;
             }
-            else
-            end= mid-1;
-         }
-         return ans;
+        
+        }
+        return ans;
     }
 };
