bfa5d8b367d1f92cbb6cb79c276c1a1fa31fca97
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-07-28 17:37:38 +0530

    Time: 98 ms (13.28%), Space: 18.6 MB (93.25%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& m, int target) {
-        // int k= m.size();
-        // int n= m[0].size();
-        // for(int i=0;i<k;i++){
-        //     int stCol=i;
-        //     int endCol=n-1;
-        //     while(stCol<=endCol){
-        //         int mid=stCol + (endCol-stCol)/2;
-        //         if(m[i][mid]==target) return true;
-        //         else if(m[i][mid]>target) endCol= mid-1;
-        //         else stCol= mid=1; 
-        //     }
-        // }
-        // return false;
-        
-         int k= m.size();
-         int n= m[0].size();
-        int row=0;
-        int col= n-1;
-        
-        while(row<k && col>=0){
-             int mid= m[row][col];
-            if(m[row][col]==target) return true;
-            else if(m[row][col]<target) row++;
-            else col--;
+        int k= m.size();
+        int n= m[0].size();
+        for(int i=0;i<k;i++){
+            int stCol=0;
+            int endCol=n-1;
+            while(stCol<=endCol){
+                int mid=stCol + (endCol-stCol)/2;
+                if(m[i][mid]==target) return true;
+                else if(m[i][mid]>target) endCol= mid-1;
+                else stCol= mid+1; 
+            }
         }
         return false;
+        
+       
     }
 };
