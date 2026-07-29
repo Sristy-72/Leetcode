93c2b4f5bfc3b68b00fcc9a7426ebe219534ddae
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-07-29 18:09:51 +0530

    Time: 0 ms (100%), Space: 15.7 MB (76.39%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
-    void allcomb(vector<int>& nums, vector<vector<int>>& ans, vector<int>& freq, vector<int> ds) {
-        if (ds.size() == nums.size()) {
-            ans.push_back(ds);
-            return;
+    void nextPermutation(vector<int>& nums) {
+        int n= nums.size();
+        int pivot=-1;
+        for(int i=n-2;i>=0;i--){
+          if(nums[i]<nums[i+1]){
+            pivot=i;
+            break;
+          }
         }
-        for (int i = 0; i < nums.size(); i++) {
-            if (!freq[i]) {
-                freq[i] = 1;
-                ds.push_back(nums[i]);
-                allcomb(nums, ans, freq, ds);
-                ds.pop_back();
-                freq[i] = 0;
+        if(pivot==-1) {
+            reverse(nums.begin(),nums.end());
+           return;
+        }  
+        for(int i=n-1;i>pivot;i--){
+            if(nums[i]>nums[pivot]) {
+            swap(nums[i],nums[pivot]);
+            break;
             }
         }
-    }
-
-    // void nextPermutation(vector<int>& nums) {
-    //     int n = nums.size();
-    //     vector<int> ds;
-    //     vector<int> freq(n, 0);
-    //     vector<vector<int>> ans;
-
-    //     // generate all permutations
-    //     allcomb(nums, ans, freq, ds);
-
-    //     // sort permutations lexicographically
-    //     sort(ans.begin(), ans.end());
-    //      ans.erase(unique(ans.begin(), ans.end()), ans.end());
-
-    //     // find current and set to next
-    //     for (int i = 0; i < ans.size(); i++) {
-    //         if (ans[i] == nums) {
-    //             if (i == ans.size() - 1) 
-    //                 nums = ans[0];      // wrap to first
-    //             else 
-    //                 nums = ans[i + 1];  // next permutation
-    //             break;
-    //         }
-    //     }
-     void nextPermutation(vector<int>& nums) {
-
-        next_permutation(nums.begin(), nums.end());
+        reverse(nums.begin()+pivot+1, nums.end());
     }
 };
