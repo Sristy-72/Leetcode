60090bc92b971353d9cbfc9a4718b2bcd6bc375a
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-07-30 09:04:04 +0530

    Time: 0 ms (100%), Space: 15.6 MB (96.6%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n= nums.size();
-        int pivot=-1;
-        for(int i=n-2;i>=0;i--){
-          if(nums[i]<nums[i+1]){
+     int n=nums.size();
+     int pivot=-1;
+     for(int i=n-2;i>=0;i--){
+        if(nums[i]<nums[i+1]){
             pivot=i;
             break;
-          }
         }
-        if(pivot==-1) {
-            reverse(nums.begin(),nums.end());
-           return;
-        }  
-        for(int i=n-1;i>pivot;i--){
-            if(nums[i]>nums[pivot]) {
+     }  
+    if(pivot==-1) return reverse(nums.begin(), nums.end());
+    // finding righmost element just bigger than pivot element
+    for(int i=n-1;i>=pivot+1;i--){
+        if(nums[i]>nums[pivot]){
             swap(nums[i],nums[pivot]);
             break;
-            }
         }
-        reverse(nums.begin()+pivot+1, nums.end());
+    }
+    reverse(nums.begin()+pivot+1, nums.end());
     }
 };
