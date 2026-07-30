53f049ab2778c0d58ed86061a4fd81290f554c69
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-07-30 09:40:37 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.79%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-     int m= matrix.size();
-     int n= matrix[0].size();
-    
-     int stRow=0;
-     int endRow=m-1;
-    
-     int stCol= 0;
-    int endCol= n-1;
-   
-     
-    while(stRow<=endRow){ // to find in the row 
-         int midRow = stRow+ (endRow-stRow)/2;
-       
-         if(matrix[midRow][0]<=target && matrix[midRow][endCol]>=target) { // this is to find in the particular row/ targeted row in which target can exist
-            int midCol= (stCol+endCol)/2;
-            if(matrix[midRow][midCol]==target) return true;
-            else if(matrix[midRow][midCol]>target)endCol= midCol-1;
-            else  stCol= midCol+1;
-           
+    bool searchMatrix(vector<vector<int>>& mt, int target) {
+        int m= mt.size();
+        int n= mt[0].size();
+        int strow=0;
+        int endrow=m-1;
+        int stcol=0;
+        int endcol= n-1;
+        while(strow<=endrow){
+            int midrow= strow+ (endrow-strow)/2;
+              if(mt[midrow][0]<=target && mt[midrow][endcol]>=target){
+                while(stcol<=endcol){
+                    int midcol= stcol+ (endcol-stcol)/2;
+                    if(mt[midrow][midcol]==target) return true;
+                    else if(mt[midrow][midcol]<target) stcol= midcol+1;
+                    else endcol= midcol-1;
+                }
+                   
+                return false;  
+            }
+            else if(mt[midrow][0]>target) endrow= midrow-1;
+            else  strow=midrow+1;  
         }
-        else if(matrix[midRow][0]<target) stRow= midRow+1;
-        else endRow= midRow-1;
-        
-       
-    }
-  return false;
+        return false;
     }
 };
