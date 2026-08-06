84d923c38ce4aede25f15077189e58e88fec74cc
Sristy <126855796+Sristy-72@users.noreply.github.com>
2026-08-06 17:00:24 +0530

    Time: 0 ms (100%), Space: 40.3 MB (45.32%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,17 +4,17 @@ public:
         int n= nums.size();
         vector<int>ans(n);
        
-       vector<int>prefix(n,1);
-       vector<int>suffix(n,1);
-       for(int i=1;i<n;i++){
-            prefix[i]= prefix[i-1]*nums[i-1];
-       }
-       for(int i=n-2;i>=0;i--){
-        suffix[i]=suffix[i+1]*nums[i+1];
-       }
+     int prefix=1;
+
        for(int i=0;i<n;i++){
-        ans[i]= prefix[i]*suffix[i];
+            ans[i]= prefix;
+            prefix=prefix*nums[i];
        }
+      int suffix=1;
+      for(int i=n-1;i>=0;i--){
+        ans[i]= suffix*ans[i];
+        suffix= suffix*nums[i];
+      }
        return ans;
     }
 };
